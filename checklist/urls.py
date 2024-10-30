35cdb201abde987b76db7e7c1f7290151b9b85ba
Promotos <promotos@gmx.de>
2024-10-30 21:36:48 +0100

    add url pattern

--- checklist/urls.py
+++ checklist/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("airplane/<int:airplane_id>/", views.airplane_detail, name="airplane_detail")
 ]
--- checklist/views.py
+++ checklist/views.py
@@ -3,3 +3,7 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the checklist index...")
+
+
+def airplane_detail(request, airplane_id: int):
+    return HttpResponse(f"Details for Airplane {airplane_id}")
