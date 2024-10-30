41b886c24b4d56b1f6fed1b5841a4dd05d448db9
Promotos <promotos@gmx.de>
2024-10-30 21:44:10 +0100

    read aircraft

--- checklist/views.py
+++ checklist/views.py
@@ -1,9 +1,15 @@
 from django.http import HttpResponse
 
+from checklist.models import Airplane
+
 
 def index(request):
     return HttpResponse("Hello, world. You're at the checklist index...")
 
 
 def airplane_detail(request, airplane_id: int):
-    return HttpResponse(f"Details for Airplane {airplane_id}")
+    try:
+        ap = Airplane.objects.get(id=airplane_id)
+        return HttpResponse(f"Details for Airplane {ap}")
+    except Airplane.DoesNotExist:
+        return HttpResponse(f"Airplane {airplane_id} does not exist")
