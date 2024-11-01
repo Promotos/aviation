c7ad618f34fd82961b921980b216f7769adc95d1
Promotos <promotos@gmx.de>
2024-11-01 12:39:04 +0100

    redirect to checklist

--- aviation/urls.py
+++ aviation/urls.py
@@ -17,7 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from . import views
+
 urlpatterns = [
+    path('', views.index, name="index"),
     path('checklist/', include("checklist.urls")),
     path('admin/', admin.site.urls),
 ]
