0d4610decddd370ffcc0aa28be46e4368e020e66
Promotos <promotos@gmx.de>
2024-11-05 21:46:07 +0100

    improve models

--- checklist/admin.py
+++ checklist/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 
-from .models import Airplane, Checklist, ChecklistItem
+from .models import Club, Airplane, Checklist, ChecklistItem, ChecklistItemGroup
 
+admin.site.register(Club)
 admin.site.register(Airplane)
 admin.site.register(Checklist)
-admin.site.register(ChecklistItem)
+
+
+class ChecklistItemAdmin(admin.ModelAdmin):
+    list_display = ["order", "group", "name"]
+    list_filter = ["group"]
+
+
+admin.site.register(ChecklistItem, ChecklistItemAdmin)
+admin.site.register(ChecklistItemGroup)
--- checklist/models.py
+++ checklist/models.py
@@ -1,28 +1,46 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class Club(models.Model):
+    name = models.CharField(max_length=50, help_text="Der Name des Vereins.")
+    users = models.ManyToManyField(User, help_text="Mitglieder des Vereins.")
+    
+    def __str__(self) -> str:
+        return f"{self.name}"
 
 
 class Airplane(models.Model):
-    name = models.CharField(max_length=50, help_text="The model name of the aircraft.")
-    callsign = models.CharField(max_length=8, help_text="The aircraft callsign.")
+    clubs = models.ManyToManyField(Club, help_text="Zu welchem Verein dieses Flugzeug gehört.")
+    name = models.CharField(max_length=50, help_text="Der Name des Flugzeugs.")
+    callsign = models.CharField(max_length=8, help_text="Das Flugzeug Callsign.")
 
     def __str__(self) -> str:
         return f"{self.name} ({self.callsign})"
 
 
 class Checklist(models.Model):
-    airplain = models.ForeignKey(Airplane, on_delete=models.CASCADE)
-    name = models.CharField(max_length=50)
+    airplain = models.ForeignKey(Airplane, on_delete=models.CASCADE, help_text="Für welches Flugzeug diese Checkliste gilt.")
+    name = models.CharField(max_length=50, help_text="Name der Checkliste.")
 
     def __str__(self) -> str:
         return f"{self.name} ({self.airplain})"
 
 
+class ChecklistItemGroup(models.Model):
+    name = models.CharField(max_length=100, help_text="Name der Gruppe.")
+
+    def __str__(self) -> str:
+        return f"{self.name}"
+
+
 class ChecklistItem(models.Model):
-    checklist = models.ForeignKey(Checklist, on_delete=models.CASCADE)
-    order = models.IntegerField(default=0)
-    name = models.CharField(max_length=100)
-    description = models.TextField(max_length=500)
-    hint = models.TextField(max_length=500)
+    checklist = models.ForeignKey(Checklist, on_delete=models.CASCADE, help_text="Zu welcher Checkliste dieser Eintrag gehört.")
+    order = models.IntegerField(default=0, help_text="Legt die Reihenfolge fest.")
+    group = models.ForeignKey(ChecklistItemGroup, on_delete=models.CASCADE, help_text="Gruppe des Eintrags.")
+    name = models.CharField(max_length=100, help_text="Name des Eintrags. Bsp. 'Rettungsgerät gesichert.'")
+    description = models.TextField(max_length=500, help_text="Beschreibung des Eintrags.", blank=True)
+    hint = models.TextField(max_length=500, help_text="Hilfestellung für diesen Eintrag", blank=True)
 
     def __str__(self) -> str:
         return f"{self.name} ({self.checklist})"
