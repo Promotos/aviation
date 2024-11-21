54d20e31b10a187634cab422b62e59aedc9e2621
Promotos <promotos@gmx.de>
2024-11-21 22:39:59 +0100

    add user boundaries check

--- checklist/models.py
+++ checklist/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.http import Http404
 from django.contrib.auth.models import User
 
 
@@ -8,6 +9,15 @@ class Club(models.Model):
     homepage = models.CharField(max_length=100, help_text="Webseite des Vereins.", blank=True)
     contact = models.CharField(max_length=100, help_text="Kontaktinformationen des Vereins.", blank=True)
     description = models.TextField(max_length=500, help_text="Beschreibung des Vereins.", blank=True)
+
+    def get(user: User, club_id: int):
+        clubs = Club.objects.filter(users__in=[user], id=club_id)
+        if clubs.count() == 0:
+            raise Http404(f"Club {club_id} not found for user {user.username}")
+        elif clubs.count() > 1:
+            raise ValueError(f"Club not unique. Got {clubs.count()}")
+        else:
+            return clubs.first()         
 
     def __str__(self) -> str:
         return f"{self.name}"
@@ -20,6 +30,15 @@ class Airplane(models.Model):
     picture_url = models.CharField(max_length=100, help_text="Bild von dem Flugzeug.", blank=True)
     description = models.TextField(max_length=500, help_text="Beschreibung des Flugzeugs.", blank=True)
 
+    def get(club: Club, airplane_id: int):
+        airplanes = Airplane.objects.filter(clubs__in=[club], id=airplane_id)
+        if airplanes.count() == 0:
+            raise Http404(f"Airplane {airplane_id} not found for club {club}")
+        elif airplanes.count() > 1:
+            raise ValueError(f"Airplane not unique. Got {airplanes.count()}")
+        else:
+            return airplanes.first()
+
     def __str__(self) -> str:
         return f"{self.name} ({self.callsign})"
 
@@ -28,6 +47,15 @@ class Checklist(models.Model):
     airplane = models.ForeignKey(Airplane, on_delete=models.CASCADE, help_text="Für welches Flugzeug diese Checkliste gilt.")
     name = models.CharField(max_length=50, help_text="Name der Checkliste.")
     description = models.TextField(max_length=500, help_text="Beschreibung der Checkliste.", blank=True)
+
+    def get(airplane: Airplane, checklist_id: int):
+        checklists = Checklist.objects.filter(airplane=airplane, id=checklist_id)
+        if checklists.count() == 0:
+            raise Http404(f"Checklist {checklist_id} not found for airplane {airplane}")
+        elif checklists.count() > 1:
+            raise ValueError(f"Checklist not unique. Got {checklists.count()}")
+        else:
+            return checklists.first()
 
     def __str__(self) -> str:
         return f"{self.name} ({self.airplane})"
@@ -47,6 +75,15 @@ class ChecklistItem(models.Model):
     name = models.CharField(max_length=100, help_text="Name des Eintrags. Bsp. 'Rettungsgerät gesichert.'")
     description = models.TextField(max_length=500, help_text="Beschreibung des Eintrags.", blank=True)
     hint = models.TextField(max_length=500, help_text="Hilfestellung für diesen Eintrag", blank=True)
+
+    def get(checklist: Checklist, checklist_item_id: int):
+        checklist_items = ChecklistItem.objects.filter(checklist=checklist, id=checklist_item_id)
+        if checklist_items.count() == 0:
+            raise Http404(f"Checklist item {checklist_item_id} not found for checklist {checklist}")
+        elif checklist_items.count() > 1:
+            raise ValueError(f"Checklist not unique. Got {checklist_items.count()}")
+        else:
+            return checklist_items.first()
 
     def __str__(self) -> str:
         return f"{self.name} ({self.checklist})"
--- checklist/views.py
+++ checklist/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpRequest
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 
 from checklist.models import Club, Airplane, Checklist, ChecklistItem
@@ -17,38 +17,44 @@ def index(request: HttpRequest):
 
 @login_required
 def club_index(request: HttpRequest, club_id: int):
-    club = Club.objects.get(id=club_id)
+    club = Club.get(user=request.user, club_id=club_id)
     airplanes = Airplane.objects.filter(clubs__in=[club_id])
+
     return render(request, "club_index.html", {"club": club, "airplanes": airplanes})
 
 
 @login_required
 def airplane_index(request: HttpRequest, club_id: int, airplane_id: int):
-    club = Club.objects.get(id=club_id)
-    airplane = Airplane.objects.get(id=airplane_id)
+    club = Club.get(user=request.user, club_id=club_id)
+    airplane = Airplane.get(club=club, airplane_id=airplane_id)
     checklists = Checklist.objects.filter(airplane=airplane)
+
     return render(request, "airplane_index.html", {"club": club, "airplane": airplane, "checklists": checklists})
 
 
 @login_required
 def checklist_detail(request: HttpRequest, club_id: int, airplane_id: int, checklist_id: int):
-    club = Club.objects.get(id=club_id)
-    airplane = Airplane.objects.get(id=airplane_id)
-    checklist = Checklist.objects.get(id=checklist_id)
+    club = Club.get(user=request.user, club_id=club_id)
+    airplane = Airplane.get(club=club, airplane_id=airplane_id)
+    checklist = Checklist.get(airplane=airplane, checklist_id=checklist_id)
     first_item = ChecklistItem.objects.filter(checklist=checklist).order_by("order").first()
-    return render(request, "checklist_detail.html", 
+
+    return render(request, "checklist_detail.html",
                   {"club": club, "airplane": airplane, "checklist": checklist, "first_item": first_item})
+
 
 @login_required
 def checklist_walk(request: HttpRequest, club_id: int, airplane_id: int, checklist_id: int, item_id: int):
-    club = Club.objects.get(id=club_id)
-    airplane = Airplane.objects.get(id=airplane_id)
-    checklist = Checklist.objects.get(id=checklist_id)
-    current_item = ChecklistItem.objects.get(id=item_id)
+    club = Club.get(user=request.user, club_id=club_id)
+    airplane = Airplane.get(club=club, airplane_id=airplane_id)
+    checklist = Checklist.get(airplane=airplane, checklist_id=checklist_id)
+    current_item = ChecklistItem.get(checklist=checklist, checklist_item_id=item_id)
+
     index = ChecklistItem.objects.order_by("order").filter(checklist=checklist, order__lte=current_item.order).count()
     count = ChecklistItem.objects.filter(checklist=checklist).count()
     last_item = ChecklistItem.objects.order_by("order").filter(checklist=checklist, order__lt=current_item.order).last()
     next_item = ChecklistItem.objects.order_by("order").filter(checklist=checklist, order__gt=current_item.order).first()
+
     return render(request, "checklist_walk.html",
                   {"club": club, "airplane": airplane, "checklist": checklist, "current_item": current_item,
                    "last_item": last_item, "next_item": next_item, "index": index, "count": count})
